bf9bbbce2b375216b19c75b2c50e14d1151ef559
WahKen <wingflokenny@gmail.com>
2022-10-16 16:12:52 +0300

    Update on the setSpeed

--- murife/lib/motor_library/motor_library.cpp
+++ murife/lib/motor_library/motor_library.cpp
@@ -1,47 +1,47 @@
 #include "motor_library.h"
 #include <AFMotor.h>
 
-void moveForward(AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void moveForward(AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
+  //motor3.run(FORWARD);
+  //motor4.run(FORWARD);
 }
 
-void moveBackward(AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void moveBackward(AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
+  //motor3.run(BACKWARD);
+  //motor4.run(BACKWARD);
 }
 
-void moveRight(AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void moveRight(AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   motor1.run(BACKWARD);
   motor2.run(BACKWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
+  //motor3.run(FORWARD);
+  //motor4.run(FORWARD);
 }
 
-void moveLeft(AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void moveLeft(AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   motor1.run(FORWARD);
   motor1.run(FORWARD);
-  motor1.run(BACKWARD);
-  motor1.run(BACKWARD);
+  //motor1.run(BACKWARD);
+  //motor1.run(BACKWARD);
 }
 
-void Stop(AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void Stop(AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   motor1.run(RELEASE);
   motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
+  //motor3.run(RELEASE);
+  //motor4.run(RELEASE);
 }
 
-void turn(boolean object, int leftIrSensor, int rightIrSensor, AF_DCMotor motor1, AF_DCMotor motor2, AF_DCMotor motor3, AF_DCMotor motor4)
+void turn(boolean object, int leftIrSensor, int rightIrSensor, AF_DCMotor motor1, AF_DCMotor motor2) //AF_DCMotor motor3, AF_DCMotor motor4)
 {
   if (object == false)
   {
@@ -58,7 +58,7 @@ void turn(boolean object, int leftIrSensor, int rightIrSensor, AF_DCMotor motor1
     }
     else
     {
-      moveForward(motor1, motor2, motor3, motor4);
+      moveForward(motor1, motor2) //motor3, motor4);
     }
   }
   else
--- murife/src/main.cpp
+++ murife/src/main.cpp
@@ -22,10 +22,10 @@ Servo myservo;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, max_distance);
 
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR34_1KHZ);
-AF_DCMotor motor4(4, MOTOR34_1KHZ);
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
+//AF_DCMotor motor3(3, MOTOR34_1KHZ);
+//AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
 // Essential Variables
 int distance = 0;
@@ -39,17 +39,19 @@ void setup()
   pinMode(irLeft, INPUT);
   pinMode(irRight, INPUT);
   myservo.attach(10);
-  myservo.write(90);
+  //myservo.write(90);
 
   //! Get rid of magic numbers by defining them elsewhere.
-  motor1.setSpeed(120);
-  motor2.setSpeed(120);
-  motor3.setSpeed(120);
-  motor4.setSpeed(120);
+  motor1.setSpeed(200);
+  motor1.run(RELEASE);
+  motor2.setSpeed(200);
+  motor2.run(RELEASE);
+  //motor3.setSpeed(200);
+  //motor4.setSpeed(200);
 }
 
 void loop()
 {
 
-  obstacleAvoidanceAlgorithm(sonar, myservo, irLeft, irRight, rightDistance, leftDistance, object, motor1, motor2, motor3, motor4);
+  obstacleAvoidanceAlgorithm(sonar, myservo, irLeft, irRight, rightDistance, leftDistance, object, motor1, motor2) //motor3, motor4);
 }
